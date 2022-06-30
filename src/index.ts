4c7d758129ce9109279fb73d70ea144605b89922
eugenehan94 <eugene.han94@gmail.com>
2022-06-30 18:46:32 -0400

    first commit of typescript tabs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const about = document.querySelector<HTMLElement>(".about");
+const btns = document.querySelectorAll<HTMLButtonElement>(".tab-btn");
+const articles = document.querySelectorAll<HTMLDivElement>(".content")
 
-import confetti from 'canvas-confetti';
+about?.addEventListener("click", (e) => {
+    const target = e.target as HTMLElement
+    const id: string | undefined = target?.dataset.id;
+    if (id){
+        // remove selected from other buttons
+        btns.forEach((btn)=> {
+            btn.classList.remove("active")
+        });
+        target?.classList.add("active")
+        
+        //hide other articles
+        articles.forEach((article) => {
+            article.classList.remove("active")
+        });
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+        const element: HTMLElement|null = document.getElementById(id);
+        element?.classList.add("active")
+    }
+})
+
+export {}
